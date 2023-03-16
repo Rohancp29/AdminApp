9246c85af2ebda0a889d662b7e5c93361bb23018
Rohan Patil <rohanpatil4002@gmail.com>
2023-03-16 16:00:05 +0530

    RP
    Initial

---
+++ app/src/main/java/com/technolite/adminapp/Add_customer.java
@@ -0,0 +1,14 @@
+package com.technolite.adminapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_customer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_customer);
+    }
+}
