01c98041fe951b8a23391e3162576d67eaa624de
Rutujapatil20 <rutujapatil202001@gmail.com>
2023-03-17 13:57:02 +0530

    Rutuja

--- app/src/main/java/com/technolite/adminapp/View_customer.java
+++ app/src/main/java/com/technolite/adminapp/View_customer.java
@@ -3,11 +3,16 @@ package com.technolite.adminapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class View_customer extends AppCompatActivity {
 
+    FloatingActionButton floatingActionButton;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -15,11 +20,19 @@ public class View_customer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_customer);
 
+        floatingActionButton = findViewById(R.id.floating_Btn);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent send = new Intent(View_customer.this, Add_customer.class);
+                startActivity(send);
+            }
+        });
+
+
         // Retrieve the title from the intent extras
         String titlev = getIntent().getStringExtra("title");
-
         TextView title=findViewById(R.id.titlev);
-
 
         // Set the title to the TextView
         title.setText(titlev);
